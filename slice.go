a762bb356e3d94eb38b9077680507314c2a31f0b
lihka1202 <88475310+lihka1202@users.noreply.github.com>
2024-05-08 15:25:40 +0800

    fix(assignment3): add sort to the code
    + made sure to sort the slice before printing anything out

--- slice.go
+++ slice.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func printSlice(input []int32) {
 	for i := 0; i < len(input); i++ {
@@ -9,15 +12,9 @@ func printSlice(input []int32) {
 }
 
 func main() {
-	count := 0
 	sli := make([]int32, 3)
 	for {
-		if count > 2 {
-			fmt.Println("\nPlease enter a value to add to the slice: ")
-		} else {
-			fmt.Println("\nPlease enter a value to add to slice (note that the initialized values of the slice are 0): ")
-		}
-
+		fmt.Println("\nPlease enter a value to add to the slice: ")
 		// Add the value to the slice somehow
 		var input int32
 		_, err := fmt.Scan(&input)
@@ -27,12 +24,8 @@ func main() {
 			return
 		}
 
-		if count > 2 {
-			sli = append(sli, input)
-		} else {
-			sli[count] = input
-			count += 1
-		}
+		sli = append(sli, input)
+		slices.Sort(sli)
 
 		//! Print the slice here
 		printSlice(sli)
